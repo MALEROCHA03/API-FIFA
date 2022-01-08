f18deb24c428dccda33f75524047e64305824d1b
MALEROCHA03 <alejalafafita@gmail.com>
2022-01-07 22:49:42 -0500

    COMMIT

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import UltimateTeam from './components/UltimateTeam'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+ const App = () =>{
+  return (
+    <div className="App">
+        <Navbar />
+        <UltimateTeam />
+        <Footer />
+    </div>
+  );
+}
+
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  const header = {
+    display: "table",
+    margin: "0 auto",
+    fontSize: "35px",
+  };
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <h3 className="navbar-brand" style={header}>
+        API FIFA
+      </h3>
+    </nav>
+  );
+};
+
+export default Navbar;
